a91dc8f9e12b77acb441c613d61df5fa67b2cb78
José Gonçalves <joseinacio774@gmail.com>
2023-06-27 10:43:08 -0300

    Cereated model contatct

---
+++ lib/models/contact_model.dart
@@ -0,0 +1,12 @@
+class ContactModel {
+  final String name;
+  final String email;
+  final String number;
+  final String image;
+  ContactModel({
+    required this.name,
+    required this.email,
+    required this.number,
+    required this.image,
+  });
+}
